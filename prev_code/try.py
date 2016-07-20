f0437e31a79595c32c2c94d7a7a9b30ca5df226c
abhigenie92 <abhigenie92@gmail.com>
2016-07-21 04:39:49 +0530

    updated main.py

--- prev_code/try.py
+++
@@ -1,5 +0,0 @@
-import xmlrpclib,datetime
-date_now=str(datetime.datetime.today())[:-10]
-
-date_created = xmlrpclib.DateTime(datetime.datetime.strptime(str(date_now), "%Y-%m-%d %H:%M"))
-print date_created
